43cdda0583f737b3502c3ad528f1b8084e0811ec
Adrien Siffermann <adrien@siffermann.me>
2016-07-08 15:35:32 +0200

    Add ServiceProviderValidatorFactory and register it in an extension

---
+++ src/GeekLearning.Domain.FluentValidation/GeekLearningDomainFluentValidationExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace GeekLearning.Domain.Validation
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.DependencyInjection.Extensions;
+
+    public static class GeekLearningDomainFluentValidationExtensions
+    {
+        public static IServiceCollection AddDomainFluentValidation(this IServiceCollection services)
+        {
+            services.TryAddSingleton<FluentValidation.IValidatorFactory, ServiceProviderValidatorFactory>();
+            services.TryAddSingleton<IValidatorFactory, ServiceProviderValidatorFactory>();
+
+            return services;
+        }
+    }
+}
---
+++ src/GeekLearning.Domain.FluentValidation/ServiceProviderValidatorFactory.cs
@@ -0,0 +1,35 @@
+﻿namespace GeekLearning.Domain.Validation
+{
+    using Domain;
+    using System;
+
+    public class ServiceProviderValidatorFactory : FluentValidation.ValidatorFactoryBase, IValidatorFactory
+    {
+        private readonly IServiceProvider serviceProvider;
+
+        public ServiceProviderValidatorFactory(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+
+        public override FluentValidation.IValidator CreateInstance(Type validatorType)
+        {
+            return this.serviceProvider.GetService(validatorType) as FluentValidation.IValidator;
+        }
+
+        IValidator IValidatorFactory.GetValidator(Type validatorType)
+        {
+            if (!validatorType.IsSubclassOfRawGeneric(typeof(DomainValidator<>)))
+            {
+                throw new NotSupportedException("The validator must be a DomainValidator.");
+            }
+
+            return this.serviceProvider.GetService(validatorType) as IValidator;
+        }
+
+        IValidator<T> IValidatorFactory.GetValidator<T>()
+        {
+            return (IValidator<T>)(this as IValidatorFactory).GetValidator(typeof(T));
+        }
+    }
+}
