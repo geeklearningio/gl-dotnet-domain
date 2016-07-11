dc3f3d6edb53262fe78cb4762a8fe5e92cbedab5
Adrien Siffermann <adrien@siffermann.me>
2016-07-11 16:42:38 +0200

    Add ModelState extensions and extend validation failure to include the property name

--- src/GeekLearning.Domain/Explanations/ValidationFailure.cs
+++ src/GeekLearning.Domain/Explanations/ValidationFailure.cs
@@ -2,10 +2,28 @@
 {
     using Validation;
 
-    public class ValidationFailure<TAggregate> : Explanation
+    public class ValidationFailure : Explanation
     {
         public ValidationFailure(IValidationFailure failure)
-            : base(failure.ToString(), $"AggregateType : { typeof(TAggregate).FullName }")
+            : base(failure.ToString())
+        {
+            this.Failure = failure;
+        }
+
+        public ValidationFailure(IValidationFailure failure, string internalMessage)
+            : base(failure.ToString(), internalMessage)
+        {
+            this.Failure = failure;
+        }
+
+        public IValidationFailure Failure { get; }
+    }
+
+
+    public class ValidationFailure<TAggregate> : ValidationFailure
+    {
+        public ValidationFailure(IValidationFailure failure)
+            : base(failure, $"AggregateType : { typeof(TAggregate).FullName }")
         {
         }
     }
