545d983939b528a93d6c9390fa3189e404d4ed61
Anna Yafi <annayafi@gmail.com>
2016-09-30 15:24:57 +0200

    rename

--- src/GeekLearning.Domain.EntityFramework/ContextAggregateBase.cs
+++ src/GeekLearning.Domain.EntityFramework/ContextAggregateBase.cs
@@ -3,13 +3,13 @@ using GeekLearning.Domain.Validation;
 
 namespace GeekLearning.Domain.EntityFramework
 {
-    public class ContextBaseAggregate<TDomain, TEntity, TUser, TValidator> : ValidatableAggregateBase<TDomain, TEntity, TUser, TValidator>
+    public class ContextAggregateBase<TDomain, TEntity, TUser, TValidator> : ValidatableAggregateBase<TDomain, TEntity, TUser, TValidator>
         where TDomain : ContextDomainBase<TUser>
         where TUser : class, IAggregate
         where TEntity : class
         where TValidator : IValidator
     {
-        public ContextBaseAggregate(TDomain domain, TEntity entity) : base(domain, entity)
+        public ContextAggregateBase(TDomain domain, TEntity entity) : base(domain, entity)
         {
             Domain.Register(this);
         }
