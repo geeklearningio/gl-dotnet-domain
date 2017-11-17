9fba2945e8052dece279d7757a0f8658326a3d1f
Cyprien Autexier <2660262+sandorfr@users.noreply.github.com>
2017-11-17 02:33:46 +0100

    add logging upon success or failure

--- src/GeekLearning.Domain.AspnetCore/DomainUserRequirementHandler.cs
+++ src/GeekLearning.Domain.AspnetCore/DomainUserRequirementHandler.cs
@@ -1,16 +1,19 @@
 ﻿namespace GeekLearning.Domain.AspnetCore
 {
     using Microsoft.AspNetCore.Authorization;
+    using Microsoft.Extensions.Logging;
     using System.Collections.Generic;
     using System.Threading.Tasks;
 
     public class DomainUserRequirementHandler : AuthorizationHandler<DomainUserRequirement>
     {
         private readonly IEnumerable<IIdentityDomain> identityDomains;
+        private readonly ILogger<DomainUserRequirementHandler> logger;
 
-        public DomainUserRequirementHandler(IEnumerable<IIdentityDomain> identityDomains)
+        public DomainUserRequirementHandler(IEnumerable<IIdentityDomain> identityDomains, ILogger<DomainUserRequirementHandler> logger)
         {
             this.identityDomains = identityDomains;
+            this.logger = logger;
         }
 
         protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, DomainUserRequirement requirement)
@@ -19,6 +22,7 @@
               || context.User.Identity == null
               || !context.User.Identity.IsAuthenticated)
             {
+                this.logger.LogInformation($"Failed to provide Identity to the domain. IsAuthenticated: {context.User?.Identity?.IsAuthenticated}, Name: {context.User?.Identity?.Name}");
                 foreach (var domain in identityDomains)
                 {
                     domain.AsAnonymous();
@@ -26,6 +30,7 @@
             }
             else
             {
+                this.logger.LogInformation($"Successfuly provided Identity to the domain. Name: {context.User?.Identity?.Name}");
                 var principal = context.User;
                 foreach (var domain in identityDomains)
                 {
