f2991abe4f3f2c35b14750c06b3dd8cdb59fe71a
Anna Yafi <annayafi@gmail.com>
2019-10-20 19:10:03 +0200

    remove comment

--- samples/GeekLearning.Domain.WebSamples/Startup.cs
+++ samples/GeekLearning.Domain.WebSamples/Startup.cs
@@ -40,7 +40,6 @@
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
-            //loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
