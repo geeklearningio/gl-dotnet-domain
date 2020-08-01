a32db1b9af83d950c2a5bed871d1d3bf658089e9
Cyprien Autexier <2660262+sandorfr@users.noreply.github.com>
2020-08-01 23:54:26 +1000

    add MapAsync extensions

--- src/GeekLearning.Domain.Primitives/Extensions/MaybeExtensions.cs
+++ src/GeekLearning.Domain.Primitives/Extensions/MaybeExtensions.cs
@@ -1,5 +1,6 @@
 ﻿namespace GeekLearning.Domain
 {
+    using System;
     using System.Threading.Tasks;
 
     public static class MaybeExtensions
@@ -7,6 +8,62 @@
         public async static Task<T> ValueAsync<T>(this Task<Maybe<T>> maybeTask) where T : class
         {
             return (await maybeTask).Value;
+        }
+
+        public async static Task<Maybe<R>> MapAsync<T, R>(this Task<Maybe<T>> maybeTask, Func<T, R> transform) where T : class where R : class
+        {
+            var maybe = await maybeTask;
+
+            if (maybe.HasValue)
+            {
+                return transform(maybe.Value);
+            }
+            else
+            {
+                return maybe.Explanation;
+            }
+        }
+
+        public async static Task<Maybe<R>> MapAsync<T, R>(this Task<Maybe<T>> maybeTask, Func<T, Maybe<R>> transform) where T : class where R : class
+        {
+            var maybe = await maybeTask;
+
+            if (maybe.HasValue)
+            {
+                return transform(maybe.Value);
+            }
+            else
+            {
+                return maybe.Explanation;
+            }
+        }
+
+        public async static Task<Maybe<R>> MapAsync<T, R>(this Task<Maybe<T>> maybeTask, Func<T, Task<Maybe<R>>> transform) where T : class where R : class
+        {
+            var maybe = await maybeTask;
+
+            if (maybe.HasValue)
+            {
+                return await transform(maybe.Value);
+            }
+            else
+            {
+                return maybe.Explanation;
+            }
+        }
+
+        public async static Task<Maybe<R>> MapAsync<T, R>(this Task<Maybe<T>> maybeTask, Func<T, Task<R>> transform) where T : class where R : class
+        {
+            var maybe = await maybeTask;
+
+            if (maybe.HasValue)
+            {
+                return await transform(maybe.Value);
+            }
+            else
+            {
+                return maybe.Explanation;
+            }
         }
     }
 }
